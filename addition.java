39288240d29078b78cb7e6a9dbc16d22edad8a6b
nikitha <nikithasaipatel@gmail.com>
2024-12-10 09:37:52 +0530

    addition in java

---
+++ addition.java
@@ -0,0 +1,15 @@
+public class addition {
+
+    public static void main(String[] args) {
+        // Hardcoded values for the two numbers
+        int number1 = 10;
+        int number2 = 20;
+       
+        // Performing addition
+        int sum = number1 + number2;
+       
+        // Displaying the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
+
